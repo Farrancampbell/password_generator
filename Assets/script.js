3af8315466dbb912d4d461e754815285a18e1c0e
Farrancampbell <farranerinandcrew@gmail.com>
2021-03-20 20:06:01 -0400

    added first function to JS code

--- Assets/script.js
+++ Assets/script.js
@@ -1,13 +1,38 @@
-
+// Input Variables 
 var enter;
-var confirmNumber;
-var confirmCharacter;
-var confirmUppercase;
-var confirmLowercase;
+var numberConfirm;
+var characterConfirm;
+var upperCaseConfirm;
+var lowerCaseConfirm;
 
-
+// Array of Characters
 character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
-// Numeric characters
+// Array of Numeric characters
 number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-// Alphabetical characters
+// Array of Alphabetical characters
 alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+// Function that converts all letters to uppercase 
+var toUpper = function (x) {
+    return x.toUpperCase(); };
+
+var choices;
+space = [];
+
+function generatePassword() {
+    // Asks for user input
+    enter = parseInt(prompt("How many characters would you like your password? Choose between 8 and 128"));
+    // First if statement for user validation 
+    if (!enter) {
+        alert("This needs a value");
+    } else if (enter < 8 || enter > 128) {
+        // Validates user input
+        // Start user input prompts
+        enter = parseInt(prompt("You must choose between 8 and 128"));
+
+    } else {
+        // Continues once user input is validated
+        numberConfirm = confirm("Will this contain numbers?");
+        characterConfirm = confirm("Will this contain special characters?");
+        upperCaseConfirm = confirm("Will this contain Uppercase letters?");
+        lowerCaseConfirm= confirm("Will this contain Lowercase letters?"); }
